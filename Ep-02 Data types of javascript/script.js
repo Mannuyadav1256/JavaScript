252758c6fb760c818e8f4b302369e0c6920e79f9
Mannuyadav1256 <142792791+Mannuyadav1256@users.noreply.github.com>
2025-02-20 14:54:50 +0530

    Add files via upload

---
+++ Ep-02 Data types of javascript/script.js
@@ -0,0 +1,51 @@
+//data type in javascript
+
+//string data type - sequence of character
+const username = 'harry';
+
+//number data type
+const userage = 30;
+
+//boolean data type
+const isuseradult = true;
+
+//udefined
+let name;
+
+//null
+let name1 = null;
+
+//sumbol data type
+let uniqueId = Symbol("id");
+
+//convert string to number
+let str ='1232';
+let num = parseInt(str);
+let num2 = Number(str);
+let num3 = +'1232';
+
+//convert number to string
+let number = 567;
+let str1 = String(number);
+let str2 = number.toString();
+let str3 = number + '';
+let str4 = `${number}`;
+
+//convert null to number
+let nullvalue = null;
+let numbervalue = Number(nullvalue);
+
+//convert undifined to number
+let undefinedvalue;
+let numvalue = Number(undefinedvalue);
+let numvalue1 = +undefinedvalue;
+let numvalue2 = parseInt(undefinedvalue);
+
+//undefined to string
+let value;
+let string = String(value)
+let string1 = value+"";
+let string2 = value.toString();
+let string3 = `${value}`;
+
+
